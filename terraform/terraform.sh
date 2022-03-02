93acf1f5d4bc6c7d39225676ff501f41f1723b14
bhikl <andreitrockii@gmail.com>
2022-03-02 19:56:36 +0300

    14

--- terraform/terraform.sh
+++ terraform/terraform.sh
@@ -6,6 +6,7 @@ echo "Installing tfswitch locally"
 
 # Get the installer on to your machine
 wget -N -c https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh
+wget -N -c https://github.com/hashicorp/terraform/archive/refs/tags/v1.1.6.zip
 
 # Make installer executable
 chmod 755 install.sh
